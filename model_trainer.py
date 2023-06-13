7ff011f18b8f0d725e9867c5dbbd23e97a09569d
jsalz <jai.salzwedel@gmail.com>
2023-06-13 14:46:43 -0400

    Flesh out the model_trainer file. Add more instance attributes for storing training/testing data, an output directory, and any models that might be generated. Added a path_for_results setter that will make an output directory if one does not exist. Update the train_and_predict function to now create a Model object. Urgh - update some logger messages to use %s rather than f-strings  :'(

--- model_trainer.py
+++ model_trainer.py
@@ -4,10 +4,12 @@ import openai
 import pandas as pd
 import sys
 
+from pathlib import Path
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import LinearSVC
 
+from model import Model
 
 logger = logging.getLogger()
 
@@ -16,52 +18,68 @@ class ModelTrainer:
     """
 
     """
-    def __init__(self, df):
+    def __init__(self, df, path_for_results=Path()):
         self.df = df.copy()
+        # A directory in which to save results. Defaults to the current working directory.
+        self.path_for_results = path_for_results
+        # Lists of the training and test data for the model.
+        self._x_train, self._x_test, self._y_train, self._y_test = self.split()
+        self.models = []
 
+    @property
+    def path_for_results(self):
+        return self._path_for_results
 
-    @staticmethod
-    def split(df):
+    @path_for_results.setter
+    def path_for_results(self, value):
+        if not isinstance(value, Path):
+            logger.error('"%s" is not a Path. Please provide a Path for saving model results.' % value)
+            self._path_for_results = None
+        else:
+            # If the Path doesn't currently exist, create it.
+            if not value.exists():
+                logger.info('Output directory "%s" doesn\'t exist. Creating it now.' % value)
+                value.mkdir(parents=True)
+            self._path_for_results = value
+
+    def split(self, independent_var_column='embedding', dependent_var_column='specialty'):
         """
         Create train and test sets with an 80/20 split.
         Args:
             df:
+            independent_var_column (str): Column name in `df` for the independent variable (x).
+            dependent_var_column (str): Column name in `df` for the dependent variable (y).
 
         Returns:
 
         """
         # split data into train and test
         x_train, x_test, y_train, y_test = train_test_split(
-            list(df['embedding'].values), df['specialty'], test_size=0.2, random_state=42
+            list(self.df[independent_var_column].values), self.df[dependent_var_column], test_size=0.2, random_state=42
         )
         return x_train, x_test, y_train, y_test
 
-    @staticmethod
-    def train_and_predict(x_train, x_test, y_train, y_test, classifier):
+    def train_a_model(self, classifier):
         """
 
         ... train a classifier using the default parameters. This is handy if you want to do a quick survey of multiple
         classifiers before settling on one to tune the hyperparameters.
 
         Args:
-            x_train:
-            x_test:
-            y_train:
-            y_test:
+
             classifier:
 
         Returns:
 
         """
-        clf = classifier()
-        clf.fit(x_train, y_train)
-        preds = clf.predict(x_test)
+        # Instantiate a Model with a new classifier.
+        model = Model(classifier())
+        model.classifier.fit(self._x_train, self._y_train)
+        model.predictions = model.classifier.predict(self._x_test)
 
-        report = classification_report(y_test, preds)
-        print(report)
-
-        with open(f'{classifier.__class__.__name__}_report.txt', mode='a') as f:
-            f.write(report)
+        model.report = classification_report(self._y_test, model.predictions)
+        model.write_report(self.path_for_results)
+        return model
 
     @staticmethod
     def tune_linearsvc_hyperparameters(x_train, x_test, y_train, y_test):
@@ -109,4 +127,4 @@ class ModelTrainer:
         if isinstance(value, pd.DataFrame):
             self._df = value
         else:
-            logger.error(f'Attempted to set df to {value}. df must be a pandas DataFrame.')
+            logger.error('Attempted to set df to %s. df must be a pandas DataFrame.' % value)
