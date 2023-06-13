2f7f04ae15f52f3b23858a8fc1cb92ba10cc90fc
jsalz <jai.salzwedel@gmail.com>
2023-06-13 14:41:26 -0400

    Create a Model class for holding onto information about classifier models.

---
+++ model.py
@@ -0,0 +1,22 @@
+import logging
+
+from pathlib import Path
+
+logger = logging.getLogger()
+
+
+class Model:
+    """
+    Class for holding information about a classifier model, such as the model itself, any predictions it has made on
+    training data, and the classification report that describes its accuracy.
+    """
+    def __init__(self, classifier):
+        self.classifier = classifier
+        self.predictions = None
+        self.classification_report = None
+
+    def write_report(self, output_path):
+        output_filename = Path(output_path, f'{self.classifier.__class__.__name__}_report.txt')
+        logger.info(f'Writing classification report to {output_filename}')
+        with open(output_filename, mode='a') as f:
+            f.write(self.classification_report)
